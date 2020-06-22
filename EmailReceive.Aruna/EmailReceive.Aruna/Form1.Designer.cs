123c6004d34f0f92a07c95405e5da15419bfac2b
Serdar <srdrflk@hotmail.com>
2020-06-22 22:26:02 +0300

    EmailReceiveWinform

---
+++ EmailReceive.Aruna/EmailReceive.Aruna/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace EmailReceive.Aruna
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSifre = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.lblAttach = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstMsg = new System.Windows.Forms.ListView();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtSifre
+            // 
+            this.txtSifre.Location = new System.Drawing.Point(97, 47);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(217, 20);
+            this.txtSifre.TabIndex = 15;
+            this.txtSifre.UseSystemPasswordChar = true;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(97, 12);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(217, 20);
+            this.txtEmail.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Şifre : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Email adress :";
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(475, 34);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(0, 13);
+            this.lblMsg.TabIndex = 11;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(368, 20);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(101, 40);
+            this.btnRead.TabIndex = 10;
+            this.btnRead.Text = "read todays mails";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // lblAttach
+            // 
+            this.lblAttach.AutoSize = true;
+            this.lblAttach.Location = new System.Drawing.Point(126, 409);
+            this.lblAttach.Name = "lblAttach";
+            this.lblAttach.Size = new System.Drawing.Size(60, 13);
+            this.lblAttach.TabIndex = 19;
+            this.lblAttach.Text = "attachment";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Son 24 saat içinde gelen kutusu";
+            // 
+            // lstMsg
+            // 
+            this.lstMsg.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstMsg.FullRowSelect = true;
+            this.lstMsg.HideSelection = false;
+            this.lstMsg.Location = new System.Drawing.Point(12, 98);
+            this.lstMsg.Name = "lstMsg";
+            this.lstMsg.Size = new System.Drawing.Size(934, 285);
+            this.lstMsg.TabIndex = 17;
+            this.lstMsg.UseCompatibleStateImageBehavior = false;
+            this.lstMsg.SelectedIndexChanged += new System.EventHandler(this.lstMsg_SelectedIndexChanged);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(19, 389);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(101, 52);
+            this.btnLoad.TabIndex = 16;
+            this.btnLoad.Text = "load attachments";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(568, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(212, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Mailin içeriğini okumak için ilgili maili seçiniz.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(988, 479);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblAttach);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstMsg);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.txtSifre);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.btnRead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSifre;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Label lblAttach;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView lstMsg;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ EmailReceive.Aruna/EmailReceive.Aruna/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Exchange.WebServices.Data;
+
+namespace EmailReceive.Aruna
+{
+    public partial class Form1 : Form
+    {
+        ExchangeService exchange = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lstMsg.Clear();
+            lstMsg.View = View.Details;
+            lstMsg.Columns.Add("Tarih", 150);
+            lstMsg.Columns.Add("Gönderen", 250);
+            lstMsg.Columns.Add("Konu", 300);
+            lstMsg.Columns.Add("Ekli Dosya", 50);
+            lstMsg.Columns.Add("İçerik", 400);
+            lstMsg.Columns.Add("Id", 50);
+            lstMsg.FullRowSelect = true;
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            ConnectToExchangeServer();
+
+            TimeSpan ts = new TimeSpan(0, -24, 0, 0);
+            DateTime date = DateTime.Now.Add(ts);
+            SearchFilter.IsGreaterThanOrEqualTo filter = new SearchFilter.IsGreaterThanOrEqualTo(ItemSchema.DateTimeReceived, date);
+
+            if (exchange != null)
+            {
+                FindItemsResults<Item> findResults = exchange.FindItems(WellKnownFolderName.Inbox, filter, new ItemView(50));
+
+                if (findResults != null)
+                {
+                    foreach (Item item in findResults)
+                    {
+
+                        EmailMessage message = EmailMessage.Bind(exchange, item.Id);
+                        ListViewItem listitem = new ListViewItem(new[]
+                        {
+                        message.DateTimeReceived.ToString(), message.From.Name.ToString() + "(" + message.From.Address.ToString() + ")", message.Subject, ((message.HasAttachments) ? "Var" : "Yok"),HtmlToPlainText(message.Body), message.Id.ToString()
+                    });
+                        lstMsg.Items.Add(listitem);
+
+                    }
+
+                    if (findResults.Items.Count <= 0)
+                    {
+                        lstMsg.Items.Add("Mail Bulunamadı!!");
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Hatalı sunucu!");
+                }
+            }
+        }
+
+        static string HtmlToPlainText(string html)
+        {
+            string buf;
+            string block = "address|article|aside|blockquote|canvas|dd|div|dl|dt|" +
+              "fieldset|figcaption|figure|footer|form|h\\d|header|hr|li|main|nav|" +
+              "noscript|ol|output|p|pre|section|table|tfoot|ul|video";
+
+            string patNestedBlock = $"(\\s*?</?({block})[^>]*?>)+\\s*";
+            buf = Regex.Replace(html, patNestedBlock, "\n", RegexOptions.IgnoreCase);
+
+            // Replace br tag to newline.
+            buf = Regex.Replace(buf, @"<(br)[^>]*>", "\n", RegexOptions.IgnoreCase);
+
+            // (Optional) remove styles and scripts.
+            buf = Regex.Replace(buf, @"<(script|style)[^>]*?>.*?</\1>", "", RegexOptions.Singleline);
+
+            // Remove all tags.
+            buf = Regex.Replace(buf, @"<[^>]*(>|$)", "", RegexOptions.Multiline);
+
+            // Replace HTML entities.
+            buf = WebUtility.HtmlDecode(buf);
+            return buf;
+        }
+
+        public void ConnectToExchangeServer()
+        {
+
+            lblMsg.Text = "Sunucuya bağlanıyor..";
+            lblMsg.Refresh();
+            try
+            {
+                exchange = new ExchangeService(ExchangeVersion.Exchange2007_SP1);
+                exchange.Credentials = new WebCredentials(txtEmail.Text, txtSifre.Text, "AutodiscoverUrl");
+                exchange.AutodiscoverUrl(txtEmail.Text);
+
+                lblMsg.Text = "Bağlanılan sunucu : " + exchange.Url.Host;
+                lblMsg.Refresh();
+
+            }
+            catch (Exception ex)
+            {
+                lblMsg.Text = "Sunucuya bağlanırken haata oluştu !!" + ex.Message;
+                lblMsg.Refresh();
+            }
+
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (exchange != null)
+            {
+                if (lstMsg.Items.Count > 0)
+                {
+                    ListViewItem item = lstMsg.SelectedItems[0];
+
+                    if (item != null)
+                    {
+                        string msgid = item.SubItems[5].Text.ToString();
+                        EmailMessage message = EmailMessage.Bind(exchange, new ItemId(msgid));
+                        if (message.HasAttachments && message.Attachments[0] is FileAttachment)
+                        {
+                            FileAttachment fileAttachment = message.Attachments[0] as FileAttachment;
+                            //Change the below Path   
+                            fileAttachment.Load(
+                            "C:\\Users\\Serdar\\source\repos\\EmailReceive\\ReadMailFromExchangeServer" + fileAttachment.Name);
+                            lblAttach.Text = "Attachment Downloaded : " + fileAttachment.Name;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Ekli dosya bulunamadı!!");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Lütfen bir maili seçiniz!!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Mail yüklenemedi!!");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Mail sunucusuna bağlanamadı!!");
+            }
+        }
+
+        private void lstMsg_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstMsg.SelectedItems.Count > 0)
+            {
+                ListViewItem item = lstMsg.SelectedItems[0];
+
+                MessageBox.Show(item.SubItems[4].Text);
+            }
+
+        }
+    }
+}
